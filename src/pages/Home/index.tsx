b581ae8c77e5f5860fb6e5606fab641b34658fbc
Thiago Raniery Ignacio Brito <thiagobritotrs@gmail.com>
2022-01-21 18:56:16 -0300

    feat: mobile-first homepage

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,55 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './styles.scss';
 
 function Home() {
-  return <h1>Hello, World!</h1>;
+  return (
+    <>
+      <header>
+        <h2 className="logo">Memento</h2>
+
+        <nav>
+          <ul>
+            <li>
+              <Link to="/score">Minhas pontuações</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <button className="switcher" type="button">
+          <span />
+        </button>
+      </header>
+      <main>
+        <div className="menu-card">
+          <h1>Menu</h1>
+
+          <div className="option-group">
+            <label htmlFor="player-name">Nome do jogador:</label>
+            <input
+              type="text"
+              id="player-name"
+              name="player-name"
+              placeholder="mastermind"
+            />
+          </div>
+          <div className="option-group">
+            <p>Quantidade de pares:</p>
+
+            <div className="pairs-selector-container">
+              <button type="button">2</button>
+              <button type="button">8</button>
+              <button type="button">18</button>
+            </div>
+          </div>
+
+          <button type="button" className="start-game">
+            Jogar
+          </button>
+        </div>
+      </main>
+    </>
+  );
 }
 
 export default Home;
