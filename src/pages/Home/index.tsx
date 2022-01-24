46659fd30f7ebbf5342c991b0eb32c3b030f425d
Thiago Raniery Ignacio Brito <thiagobritotrs@gmail.com>
2022-01-24 18:46:24 -0300

    refactor: update homepage layout

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,37 +1,45 @@
 import React from 'react';
 import Header from '../../components/Header';
-import './styles.scss';
+import PlayIcon from '../../assets/play.svg';
+import styles from './styles.module.scss';
 
 function Home() {
   return (
     <>
       <Header />
-      <main>
-        <div className="menu-card">
-          <h1>Menu</h1>
+      <main className={styles['page-content']}>
+        <div className={styles['menu-card']}>
+          <h1 className={styles['main-title']}>Menu</h1>
 
-          <div className="option-group">
-            <label htmlFor="player-name">Informe o nome do seu jogador:</label>
-            <input
-              type="text"
-              id="player-name"
-              name="player-name"
-              placeholder="mastermind"
-            />
-          </div>
-          <div className="option-group">
-            <p>Escolha a quantidade de pares:</p>
+          <div className={styles['menu-content']}>
+            <div>
+              <div className={styles['option-group']}>
+                <label htmlFor="player-name">
+                  Digite o nome do seu jogador:
+                </label>
+                <input
+                  type="text"
+                  id="player-name"
+                  name="player-name"
+                  placeholder="Mastermind"
+                />
+              </div>
+              <div className={styles['option-group']}>
+                <p>Escolha a quantidade de pares:</p>
 
-            <div className="pairs-selector-container">
-              <button type="button">2</button>
-              <button type="button">8</button>
-              <button type="button">18</button>
+                <div className={styles['pairs-selector-container']}>
+                  <button type="button">2</button>
+                  <button type="button">8</button>
+                  <button type="button">18</button>
+                </div>
+              </div>
             </div>
-          </div>
 
-          <button type="button" className="start-game">
-            Jogar
-          </button>
+            <button type="button" className={styles['start-game']}>
+              <p className="sr-only">Jogar</p>
+              <img src={PlayIcon} alt="" />
+            </button>
+          </div>
         </div>
       </main>
     </>
