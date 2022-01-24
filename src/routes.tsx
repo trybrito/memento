a0e934651e360872706d26210c50715d890dd15b
Thiago Raniery Ignacio Brito <thiagobritotrs@gmail.com>
2022-01-24 18:49:03 -0300

    refactor: update application routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import Score from './pages/Score';
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="score" element={<Score />} />
       </Routes>
     </BrowserRouter>
   );
